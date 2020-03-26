867641f17f207b75bce4beda945603a28b1b66e6
Saleh Ahmad Khan <43320252+saleh20185@users.noreply.github.com>
2020-03-26 21:22:49 +0600

    Update settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['arcane-crag-09760.herokuapp.com']
+ALLOWED_HOSTS = ['arcane-crag-09760.herokuapp.com','whispering-inlet-84512.herokuapp.com']
 
 
 # Application definition
